80d7ba9f95790562c1c7311ddcbd339fd38191b7
choco-develop <onoigboriaose@gmail.com>
2023-11-23 22:06:40 +0100

    first

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const Port = process.env.PORT || 3000;
+
+const dataAnalyst = {
+  senior: {
+    name: "Peter",
+    age: 27,
+    location: "Lagos",
+  },
+  junior: {
+    name: "Gregory",
+    age: 24,
+    location: "Portharcourt",
+  },
+  midlevel: {
+    name: "Anita",
+    age: 31,
+    location: "Jos",
+  },
+};
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/api/:name", (req, res) => {
+  const position = req.params.name.toLowerCase();
+  if (dataAnalyst[position]) {
+    res.send(dataAnalyst[position]);
+  } else {
+    res.send("NOT AVAILABLE");
+  }
+  //res.json(dataAnalyst);
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+app.listen(Port, () => console.log("listening on port 3000"));
