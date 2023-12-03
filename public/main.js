73133f3ee6ff34f1a73aa9d115f072fcc2818c3c
choco-develop <onoigboriaose@gmail.com>
2023-12-03 17:37:21 +0100

    client-side-js-code

--- public/main.js
+++ public/main.js
@@ -4,7 +4,7 @@ async function apiRequest() {
   const rapperName = document.querySelector("input").value;
   try {
     const response = await fetch(
-      `https://rap-names-api-fun.herokuapp.com/api/${rapperName}`
+      `https://job-board-lmpt.onrender.com/api/${rapperName}`
     );
     const data = await response.json();
 
